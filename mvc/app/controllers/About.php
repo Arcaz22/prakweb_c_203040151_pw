6fd403609ba12d560b7aa76b802e72a8221ca64c
Arcaz22 <chandraarcychan@gmail.com>
2022-10-10 21:24:56 +0700

    vidio 6 - Assets

--- mvc/app/controllers/About.php
+++ mvc/app/controllers/About.php
@@ -7,18 +7,18 @@ class About extends Controller {
     $data['pekerjaan'] = $pekerjaan;
     $data['umur'] = $umur;
     $data['judul'] = 'About Me';
-    $this->view('templates/header');
+    $this->view('templates/header', $data);
     $this->view('about/index', $data);
     $this->view('templates/footer');
-  }
+}
 
-  public function page()
-  {
+public function page()
+{
     $data['judul'] = 'Pages';
     $this->view('templates/header', $data);
     $this->view('about/page');
     $this->view('templates/footer');
-  }
+}
 }
 
 ?>
---
+++ mvc/app/core/Constants.php
@@ -0,0 +1,5 @@
+<?php 
+
+define('BASEURL', 'http://localhost/prakweb_c_203040151_pw/mvc/public');
+
+?>
