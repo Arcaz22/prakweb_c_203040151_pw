4df1e233c881804b1f65d4771ea05c1f6155158c
Arcaz22 <chandraarcychan@gmail.com>
2022-10-10 23:31:25 +0700

    vidio 8 - Database Wrapper

--- mvc/app/core/Constants.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-define('BASEURL', 'http://localhost/prakweb_c_203040151_pw/mvc/public');
-
-?>
--- mvc/app/views/mahasiswa/index.php
+++ mvc/app/views/mahasiswa/index.php
@@ -1,15 +1,16 @@
 <div class="container mt-3">  
+
   <div class="row">
-      <div class="col-lg-6">
-        <h3>Daftar Mahasiswa</h3>
-        <?php foreach( $data['mhs'] as $mhs ) : ?>
-          <ul>
-              <li><?= $mhs['nama']; ?></li>
-              <li><?= $mhs['npm']; ?></li>
-              <li><?= $mhs['email']; ?></li>
-              <li><?= $mhs['jurusan']; ?></li>
-          </ul>      
-        <?php endforeach; ?>
+    <div class="col-lg-6">
+      <h3>Daftar Mahasiswa</h3>
+      <ul class="list-group">
+      <?php foreach( $data['mhs'] as $mhs ) : ?>
+        <li class="list-group-item">
+        <?= $mhs['nama']; ?>
+          <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-primary float-lg-end">detail</a>
+        </li>
+      <?php endforeach; ?>
+      </ul>      
     </div>
   </div>
 
